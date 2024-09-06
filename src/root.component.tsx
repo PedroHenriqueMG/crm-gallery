dfe78e891484fb363a68f66fdb92da0775ca4b0d
PedroHenriqueMG <pedrohdefsouza@gmail.com>
2024-09-06 18:19:23 -0300

    feat: create page structure

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,24 @@
+import cropped from './assets/cropped.png'
+import nature from './assets/nature.png'
+
 export default function Root() {
-	return <section>gallery</section>
+	return (
+		<section>
+			<div>
+				<img src={cropped} alt='cropped' />
+				<img src={nature} alt='nature' />
+				<img src={cropped} alt='cropped' />
+			</div>
+			<div>
+				<img src={nature} alt='nature' />
+				<img src={cropped} alt='cropped' />
+				<img src={nature} alt='nature' />
+			</div>
+			<div>
+				<img src={cropped} alt='cropped' />
+				<img src={nature} alt='nature' />
+				<img src={cropped} alt='cropped' />
+			</div>
+		</section>
+	)
 }
